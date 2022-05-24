98f619c5ac81feb82a3a91451bd9e1298b6bdb8a
muhammad-umar-divaaco05 <muhammad.umar@divaaco.com>
2022-05-24 18:35:26 +0500

    adding new lines

---
+++ lists.py
@@ -0,0 +1,24 @@
+fruits = ["Apples","Mangos","Oranges","Strawberries"]
+
+# Insert in list
+fruits.insert(2,"Umar")
+# Edit in list
+fruits[0] = "Banana"
+# Append in list
+fruits.append("egg")
+names = ["ali","ahmad"]
+# fruits.append(names)
+# Remove in list
+fruits.remove('egg')
+# Pop in list
+fruits.pop(2) # just give index number of the value you want to remove
+# Sort list
+fruits.sort()
+
+# Reverse list
+fruits.reverse()
+# len of list
+print(len(fruits))
+# Get specific value in list
+
+print(fruits)
---
+++ variables.py
@@ -0,0 +1,57 @@
+# a = 9
+# name ='students'
+# cgpa = 3.3
+# male =True
+
+# print(a)
+# print(name)
+# print(cgpa)
+# print(male)
+
+
+
+# d, b, c , f = (7,"ali",True,2.5)
+
+# print(d,b,c,f)
+# print(type(c))
+
+
+# if type(d) == bool:
+#     print("I am a bool")
+# else:
+#     print("I am not a bool")
+    
+# if male == True:
+#     print("You can go to male washroom")
+# else:
+#     print("please leave sir")
+    
+    
+    
+# strings 
+# name = "Umar"
+# age = 23
+# cgpa=3.3
+# print(f'My name is {name}. and I am {age}. my cgpa is {cgpa}')
+
+
+# print("my name is {name}. and I'm {age}".format(name=name,age=age))
+
+
+x = "hello world umar"
+# y="umar"
+print(x.capitalize())
+print(x.lower())
+print(x.upper())
+print(x.swapcase())
+print(type(x))
+print(len(x))
+print(x.replace('world','umar'))
+check = 'l'
+print(x.count('l'))
+print(x.startswith('l'))
+print(x.endswith('r'))
+print(x.split())
+print(x.isalnum())
+print(x.isalpha())
+print(x.isnumeric())
